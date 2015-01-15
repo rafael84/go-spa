f3d37dc09ec07f6c02998901afb294e62fd7247e
Rafael Martins <rafael.martins@bemobi.com.br>
2015-01-15 16:24:49 -0200

    start fixing translations

--- backend/user/signup.go
+++ backend/user/signup.go
@@ -33,7 +33,7 @@ func (r *SignUpResource) POST(c *ctx.Context, rw http.ResponseWriter, req *http.
 	err := json.NewDecoder(req.Body).Decode(&form)
 	if err != nil {
 		log.Errorf("Could not parse request data: %s", err)
-		return ctx.BadRequest(rw, c.T("account.user.could_not_parse_request_data"))
+		return ctx.BadRequest(rw, c.T("user.signup.could_not_parse_request_data"))
 	}
 
 	// create new user service
@@ -41,15 +41,15 @@ func (r *SignUpResource) POST(c *ctx.Context, rw http.ResponseWriter, req *http.
 	// check whether the email address is already taken
 	_, err = service.GetByEmail(form.Email)
 	if err == nil {
-		return ctx.BadRequest(rw, c.T("account.user.email_taken"))
+		return ctx.BadRequest(rw, c.T("user.signup.email_taken"))
 	} else if err != pg.ERecordNotFound {
 		log.Errorf("Could not query user: %s", err)
-		return ctx.InternalServerError(rw, "account.user.could_not_query_user")
+		return ctx.InternalServerError(rw, "user.signup.could_not_query_user")
 	}
 
 	// password validation
 	if form.Password != form.PasswordAgain {
-		return ctx.BadRequest(rw, c.T("account.user.passwords_mismatch"))
+		return ctx.BadRequest(rw, c.T("user.signup.passwords_mismatch"))
 	}
 
 	// create new user
